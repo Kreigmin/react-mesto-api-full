33142aeb45b0acc39e1b5ba98d260ee5db4a7a64
Dmitry Sobolev <leejjuk@gmail.com>
2021-09-29 18:25:20 +0300

    feat: create middleware which enable cors

---
+++ backend/middlewares/cors.js
@@ -0,0 +1,22 @@
+const allowedCors = [
+  "https://beautiful-places.nomoredomains.club",
+  "localhost:3000",
+];
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers["access-control-request-headers"];
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+    return res.end();
+  }
+
+  next();
+};
