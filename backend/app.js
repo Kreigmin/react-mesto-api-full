9bfaa8285521238a7d8d53001e29e44e7a726b8c
Dmitry Sobolev <leejjuk@gmail.com>
2021-09-30 01:50:20 +0300

    comment non-simple cors

--- backend/app.js
+++ backend/app.js
@@ -39,20 +39,20 @@ const allowedCors = [
 
 app.use(function (req, res, next) {
   const { origin } = req.headers;
-  const requestHeaders = req.headers["Access-Control-Request-Headers"];
-  const { method } = req.method;
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  console.log([origin, requestHeaders, method]);
+  // const requestHeaders = req.headers["Access-Control-Request-Headers"];
+  // const { method } = req.method;
+  // const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  // console.log([origin, requestHeaders, method]);
 
   if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
   }
 
-  if (method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-    res.header("Access-Control-Allow-Headers", requestHeaders);
-    return res.end();
-  }
+  // if (method === "OPTIONS") {
+  //   res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+  //   res.header("Access-Control-Allow-Headers", requestHeaders);
+  //   return res.end();
+  // }
 
   next();
 });
