30d301efdb047bbe95fefe7c5f4d16d065eda5f5
Dmitry Sobolev <leejjuk@gmail.com>
2021-09-30 11:36:24 +0300

    refactor: delete if statement for checking method

--- backend/app.js
+++ backend/app.js
@@ -31,25 +31,15 @@ app.use(requestLogger);
 
 // app.use(cors);
 
-const allowedCors = [
-  "https://beautiful-places.nomoredomains.club",
-  "http://beautiful-places.nomoredomains.club",
-  "localhost:3000",
-];
-
 app.use(function (req, res, next) {
   const requestHeaders = req.headers["Access-Control-Request-Headers"];
-  const { method } = req.method;
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  // console.log([origin, requestHeaders, method]);
+  console.log(requestHeaders);
 
   res.header("Access-Control-Allow-Origin", "*");
 
-  if (method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-    res.header("Access-Control-Allow-Headers", requestHeaders);
-    return res.end();
-  }
+  res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+  res.header("Access-Control-Allow-Headers", requestHeaders, "Content-Type");
 
   next();
 });
