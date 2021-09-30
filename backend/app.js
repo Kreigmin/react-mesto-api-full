a79db7358b5db6cbc62e6a54efa9d3e5e8f15905
Dmitry Sobolev <leejjuk@gmail.com>
2021-09-30 11:18:18 +0300

    refactor: delete if statement in cors middleware

--- backend/app.js
+++ backend/app.js
@@ -38,15 +38,12 @@ const allowedCors = [
 ];
 
 app.use(function (req, res, next) {
-  const { origin } = req.headers;
   // const requestHeaders = req.headers["Access-Control-Request-Headers"];
   // const { method } = req.method;
   // const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
   // console.log([origin, requestHeaders, method]);
 
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+  res.header("Access-Control-Allow-Origin", "*");
 
   // if (method === "OPTIONS") {
   //   res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
