a9ae3e58680d25800e467d8b022f1683d8a9533d
Dmitry Sobolev <leejjuk@gmail.com>
2021-10-02 18:07:47 +0300

    refactor: change if statement for token validation in auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,8 +5,11 @@ const ForbiddenError = require("../errors/forbidden-error");
 const { NODE_ENV, JWT_SECRET } = process.env;
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
+  // if (!req.cookies.jwt) {
+  //   return next(new ForbiddenError("Необходима авторизация"));
+  // }
   if (!req.cookies.jwt) {
-    return next(new ForbiddenError("Необходима авторизация"));
+    res.end();
   }
   const token = req.cookies.jwt;
   let payload;
