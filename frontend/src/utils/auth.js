82a45b588fc150ec53da0cfddf922ff0d0690b1e
Dmitry Sobolev <leejjuk@gmail.com>
2021-10-02 19:02:31 +0300

    refactor: add property credentials for all requests

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -3,6 +3,7 @@ const BASE_URL = "https://api.beautiful-places.nomoredomains.club/";
 export function auth(email, password, endPoint) {
   return fetch(`${BASE_URL}${endPoint}`, {
     method: "POST",
+    credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
@@ -13,6 +14,7 @@ export function auth(email, password, endPoint) {
 export function getContent() {
   return fetch(`${BASE_URL}users/me`, {
     method: "GET",
+    credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
