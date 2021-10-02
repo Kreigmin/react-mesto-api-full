6655c57ded5657869ac3ae3087b5344bf3cd355f
Dmitry Sobolev <leejjuk@gmail.com>
2021-10-02 14:59:45 +0300

    refactor: uncomment if statement for cookie jwt in auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 const UnauthorizedError = require("../errors/unauthorized-error");
-// const ForbiddenError = require("../errors/forbidden-error");
+const ForbiddenError = require("../errors/forbidden-error");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  // if (!req.cookies.jwt) {
-  //   return next(new ForbiddenError("Необходима авторизация"));
-  // }
+  if (!req.cookies.jwt) {
+    return next(new ForbiddenError("Необходима авторизация"));
+  }
   const token = req.cookies.jwt;
   let payload;
   try {
